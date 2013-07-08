e90ebdd15362553710d21c1497f8bad6ba26d449
Paul Wittmann <paul@railslove.com>
2013-07-08 12:04:08 +0200

    Assets/templates: converted to SLIM

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -1,1 +1,4 @@
 Slim::Engine.set_default_options pretty: true if Rails.env.development? or Rails.env.testing?
+
+# compile slim-templates in the app/assets folder (used for Angular templates in app/assets/templates)
+Rails.application.assets.register_engine('.slim', Slim::Template)
